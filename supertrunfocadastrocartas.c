0f9114d691f30d20a592335601b4475af9135c65
539329486783495 <generalusesemail285861@gmail.com>
2025-02-20 18:51:55 -0300

    Update supertrunfocadastrocartas.c
    A

--- supertrunfocadastrocartas.c
+++ supertrunfocadastrocartas.c
@@ -13,7 +13,7 @@ int main(){
     printf("-- CARTA 1 --\n");
     printf("Digite a letra do Estado: ");
     scanf(" %c", &estado);
-    printf("Digite o codigo da carta: ");
+    printf("Digite o código da carta: ");
     scanf(" %49s", CodigoCarta);
     getchar();
 
@@ -21,16 +21,16 @@ int main(){
     fgets(NomeCidade, 45, stdin);       
     NomeCidade[strcspn(NomeCidade, "\n")] = '\0'; 
 
-    printf("Digite a Populacao: ");
+    printf("Digite a População: ");
     scanf(" %d", &populacao);
 
-    printf("Digite a area: ");
+    printf("Digite a área: ");
     scanf(" %f", &AreakmQ);
 
     printf("Digite o PIB: ");
     scanf(" %f", &PIB);
 
-    printf("Digite os pontos turisticos: ");
+    printf("Digite os pontos turísticos: ");
     scanf(" %d", &pontosturisticos);
 
     DensidadePopulacional1 = (float) populacao / AreakmQ;
@@ -47,7 +47,7 @@ int main(){
     scanf(" %c", &estado2);
     getchar();
 
-    printf("Digite o codigo da carta: ");
+    printf("Digite o código da carta: ");
     scanf(" %49s", CodigoCarta2);
 
     getchar();
@@ -55,16 +55,16 @@ int main(){
     fgets(NomeCidade2, sizeof(NomeCidade2), stdin);
     NomeCidade2[strcspn(NomeCidade2, "\n")] = '\0';
 
-    printf("Digite a Populacao: ");
+    printf("Digite a População: ");
     scanf(" %d", &populacao2);
 
-    printf("Digite a area: ");
+    printf("Digite a área: ");
     scanf(" %f", &AreakmQ2);
 
     printf("Digite o PIB: ");
     scanf(" %f", &PIB2);
 
-    printf("Digite os pontos turisticos: \n");
+    printf("Digite os pontos turísticos: \n");
     scanf(" %d", &pontosturisticos2);
  
     DensidadePopulacional2 = (float) populacao2 / AreakmQ2;
@@ -85,9 +85,6 @@ int main(){
     printf("Densidade Populacional: Carta 1 venceu (%d)\n", DensidadePopulacional1 > DensidadePopulacional2);
     printf("PIB per Capita: Carta 1 venceu (%d)\n", PIBPerCapita1 > PIBPerCapita2);
     printf("Super Poder: Carta 1 venceu (%d)\n", SuperPoder > SuperPoder2);
-
-    // while (1);  Usei esse while para conseguir abrir o código em forma de executável no terminal do windows, 
-    //estava achando muito chato ter que ler tudo no pequeno terminal do VS Code.
 
     return 0;
 }
